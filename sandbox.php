96a70d05294745ab6ca49a8febb099fafe864632
Jisse Reitsma <jisse@yireo.com>
2017-06-24 11:46:39 +0200

    Pending changes

--- sandbox.php
+++ sandbox.php
@@ -2,6 +2,5 @@
 require_once 'app/bootstrap.php';
 $bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
 
-$example = $bootstrap->getObjectManager()
-    ->create('\Psr\Log\LoggerInterface');
+$example = $bootstrap->getObjectManager()->create('\Psr\Log\LoggerInterface');
 \Zend_Debug::dump(get_class($example));
