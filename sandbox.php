defac100c261d008abdba21566ee77b7cbd7256a
Jisse Reitsma <jisse@yireo.com>
2017-06-24 11:43:38 +0200

    New file

---
+++ sandbox.php
@@ -0,0 +1,7 @@
+<?php
+require_once 'app/bootstrap.php';
+$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+
+$example = $bootstrap->getObjectManager()
+    ->create('\Psr\Log\LoggerInterface');
+\Zend_Debug::dump(get_class($example));
