598cd16f25089f9584ce18c0e97d74c5be4d1c11
Jisse Reitsma <info@yireo.com>
2018-05-28 08:49:41 +0200

    Pending changes

--- sandbox.php
+++ sandbox.php
@@ -1,6 +1,12 @@
 <?php
-require_once 'app/bootstrap.php';
-$bootstrap = \Magento\Framework\App\Bootstrap::create(BP, $_SERVER);
+declare(strict_types=1);
 
-$example = $bootstrap->getObjectManager()->create('\Psr\Log\LoggerInterface');
-\Zend_Debug::dump(get_class($example));
+use Magento\Framework\App\Bootstrap;
+use Psr\Log\LoggerInterface;
+use Zend_Debug;
+
+require_once 'app/bootstrap.php';
+$bootstrap = Bootstrap::create(BP, $_SERVER);
+
+$example = $bootstrap->getObjectManager()->create(LoggerInterface::class);
+Zend_Debug::dump(get_class($example));
