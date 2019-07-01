7c4651ce33719d10195f9caa9eca82847f6ce7d7
Jisse Reitsma <jisse@yireo.com>
2019-07-01 20:25:55 +0200

    Remove Zend_Debug because ZF1 is almost gone

--- sandbox.php
+++ sandbox.php
@@ -3,10 +3,9 @@ declare(strict_types=1);
 
 use Magento\Framework\App\Bootstrap;
 use Psr\Log\LoggerInterface;
-use Zend_Debug;
 
 require_once 'app/bootstrap.php';
 $bootstrap = Bootstrap::create(BP, $_SERVER);
 
 $example = $bootstrap->getObjectManager()->create(LoggerInterface::class);
-Zend_Debug::dump(get_class($example));
+echo get_class($example)."\n";
